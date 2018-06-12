ba1eff8088f5f1a80bcf7afea17c9ed0b914d936
vunguyenhoang89 <vunguyenhoang2011@yahoo.com>
2018-06-12 11:09:43 +0700

    up code basic

--- js/notice.js
+++ js/notice.js
@@ -1,26 +1,31 @@
-	jQuery(document).ready(function() {
-		jQuery('#system_notice_area').animate({
-			opacity : 'show',
-			height : 'show'
-		}, 500);
+jQuery(document).ready(function() {
+    jQuery('#system_notice_area').animate({
+        opacity : 'show',
+        height : 'show'
+    }, 500);
 
-		jQuery('#system_notice_area_dismiss').click(function() {
-			jQuery('#system_notice_area').animate({
-				opacity : 'hide',
-				height : 'hide'
-			}, 500);
+    jQuery('#system_notice_area_dismiss').click(function() {
+        jQuery('#system_notice_area').animate({
+            opacity : 'hide',
+            height : 'hide'
+        }, 500);
 
-		});
+    });
 
-	});
-	function XyzSmapToggleRadio(value,buttonId)
-	{
-		if (value == '1') {
-	    	jQuery("#"+buttonId+"_no").removeClass( "xyz_smap_toggle_on" ).addClass( "xyz_smap_toggle_off" );
-	    	jQuery("#"+buttonId+"_yes").removeClass( "xyz_smap_toggle_off" ).addClass( "xyz_smap_toggle_on" );
-	        }
-	    else if (value == '0') {
-	    	jQuery("#"+buttonId+"_yes").removeClass( "xyz_smap_toggle_on" ).addClass( "xyz_smap_toggle_off" );
-	    	jQuery("#"+buttonId+"_no").removeClass( "xyz_smap_toggle_off" ).addClass( "xyz_smap_toggle_on" );
-	    }
-	}
+    jQuery('#xyz_smap_twmessage').keyup(function () {
+        var text = jQuery(this).val();
+        var textLength = text.length;
+        var maxLength = 30;
+           if (textLength == maxLength) {
+               alert("ハッシュタグ欄は"+maxLength+"文字以下で入力してください。");
+           }
+    });
+    jQuery('#xyz_smap_twmessage_title').keyup(function () {
+        var text = jQuery(this).val();
+        var textLength = text.length;
+        var maxLength = 70;
+            if (textLength == maxLength) {
+                alert("ハッシュタグ欄は"+maxLength+"文字以下で入力してください。");
+            }
+    });
+});
